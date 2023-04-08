5af094e4250a4bc6e9ec7388da1d24ce8256b243
E-bata <emmilytatas@gmail.com>
2023-04-07 20:17:26 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,8 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char **argv)
+int main(int argc, char **argv[])
 {
-if (argc > 0)
-printf("%s\n", argv[0]);
-return (0);
+	printf("argc = %d\n", argc);
+	return 0;
 }
